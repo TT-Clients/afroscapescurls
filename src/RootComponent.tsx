e598062adb36b22338097cdc1c5a986f8a31e49c
Grey Elerson <grey.elerson@fauna.com>
2023-03-11 22:03:59 -0700

    Update router

--- src/RootComponent.tsx
+++ src/RootComponent.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import AboutPage from './pages/AboutPage'
+import CommunityEngagementPage from './pages/CommunityEngagementPage'
+import EducationPage from './pages/EducationPage'
 import HomePage from './pages/HomePage'
 import Intro from './pages/Intro'
+import MeetTeamPage from './pages/MeetTeamPage'
 import NotFoundPage from './pages/NotFoundPage'
+import SignatureEventsPage from './pages/SignatureEventsPage'
+import SpecialEventsPage from './pages/SpecialEventsPage'
 import { ROUTES } from './resources/routes-constants'
 import './styles/main.sass'
 
@@ -10,9 +16,15 @@ const RootComponent: React.FC = () => {
     return (
         <Router>
             <Routes>
-                <Route path="*" element={<NotFoundPage />} />
-                <Route path={ROUTES.HOMEPAGE_ROUTE} element={<Intro />} />
+                <Route path={ROUTES.INTRO_ROUTE} element={<Intro />} />
                 <Route path={ROUTES.MAINPAGE_ROUTE} element={<HomePage />} />
+                <Route path={ROUTES.ABOUT_ROUTE} element={<AboutPage />} />
+                <Route path={ROUTES.MEETTEAM_ROUTE} element={<MeetTeamPage />} />
+                <Route path={ROUTES.COMMUNITYENGAGEMENT_ROUTE} element={<CommunityEngagementPage />} />
+                <Route path={ROUTES.EDUCATION_ROUTE} element={<EducationPage />} />
+                <Route path={ROUTES.SPECIALEVENTS_ROUTE} element={<SpecialEventsPage />} />
+                <Route path={ROUTES.SIGNATUREEVENTS_ROUTE} element={<SignatureEventsPage />} />
+                <Route path="*" element={<NotFoundPage />} />
             </Routes>
         </Router>
     )
