aa61da1e4485c26bf422182598d3f8dc2165746d
Grey Elerson <grey.elerson@fauna.com>
2023-03-11 22:03:27 -0700

    Add routes for new pages

--- src/resources/routes-constants.ts
+++ src/resources/routes-constants.ts
@@ -1,4 +1,10 @@
 export const ROUTES = {
-    HOMEPAGE_ROUTE: '/',
-    MAINPAGE_ROUTE: '/home'
+    INTRO_ROUTE: '/',
+    MAINPAGE_ROUTE: '/home',
+    ABOUT_ROUTE: '/about',
+    COMMUNITYENGAGEMENT_ROUTE: '/community-engagement',
+    EDUCATION_ROUTE: '/education',
+    MEETTEAM_ROUTE: '/meet-the-team',
+    SIGNATUREEVENTS_ROUTE: '/signature-events',
+    SPECIALEVENTS_ROUTE: '/special-events'
 }
