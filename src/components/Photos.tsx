6e2c263af3e124aef647573bd87e5ff7562bd668
Terra Taylor <terra.taylor@gmail.com>
2023-04-07 15:54:13 -0500

    added steampunk link

--- src/components/Photos.tsx
+++ src/components/Photos.tsx
@@ -22,6 +22,16 @@ const Photos: React.FC = () => {
                         </div>
                     </div>
                 </div>
+                <div className="row">
+                    <div className="col mt-5">
+                        <h4 className="text-center">Join us at the next Steampunk Tea Party on <strong>June 10th 2023</strong></h4>
+                        <div className="text-center">
+                                <a href="https://www.eventbrite.com/e/steampunk-tea-party-tickets-609026824627?aff=erelexpmlt" rel="noreferrer" target="_blank" className="btn btn-danger mb-3 btn-lg">
+                                    Tickets Available Now!
+                                </a>
+                            </div>
+                    </div>
+                </div>
 
               
                
