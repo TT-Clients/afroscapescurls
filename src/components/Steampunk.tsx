beacc41c7419d5212229a6015204b5fcd1ff1de4
Terra Taylor <terra.taylor@gmail.com>
2023-08-18 15:00:45 -0500

    updates

--- src/components/Steampunk.tsx
+++ src/components/Steampunk.tsx
@@ -3,13 +3,19 @@ import React from 'react'
 const Steampunk: React.FC = () => {
 
     return (
-        <section className="page-section bg-light" id="steampunk">
+        <section className="page-section bg-light" id="steampunk" >
         <div className="container">
             <div className="text-center">
-                <h2 className="section-heading text-uppercase">Steampunk Teaparty</h2>
+                <h2 className="section-heading text-uppercase">Recent Events</h2>
             </div>
 
-            <div data-tockify-component="calendar" data-tockify-calendar="the.house.events"></div>
+          
+          <div className="row">
+            <div className="col">
+                <h3 className="text-center">Thank you for coming out and supporting us at our Juneteenth Steam Punk Tea Party!</h3>
+                <iframe width="100%" height="700" src="https://www.youtube.com/embed/_dofkJ90mxc" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+            </div>
+          </div>
            
         </div>
     </section>
